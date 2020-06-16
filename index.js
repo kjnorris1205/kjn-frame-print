7ec146881848b6447e9366f3bf36e59d8791124b
Kevin Norris <kjnorris1205@hotmail.com>
2020-06-16 00:12:34 -0400

    Make it work as script or module

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-module.exports = function print(message) {
+function print(message) {
   const starLine = '*'.repeat(message.length);
   console.log(starLine);
   console.log(message);
   console.log(starLine);
+}
+
+if (require.main == module) {
+  // Running as a script
+  print(process.argv[2]);
+} else {
+  // Being required
+  module.exports = print
 }
